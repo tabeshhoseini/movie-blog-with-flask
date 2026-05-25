5ff0c7ca73706a9743025946d5f29f41654de0e1
Tabesh <tabeshhoseini85@gmail.com>
2026-05-25 10:56:26 +0330

    Delete movieBlog directory

--- movieBlog/run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Installing dependencies...
-pip install -r requirements.txt
-echo.
-echo Starting Movie Blog...
-python app.py
-pause
--- movieBlog/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Installing dependencies..."
-pip install -r requirements.txt
-echo ""
-echo "Starting Movie Blog..."
-python app.py
